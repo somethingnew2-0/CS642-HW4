ba600a0bcfeb15a61ffe604c3fded01b535166e2
Peter Collins <peter@drifty.com>
2014-04-21 00:15:41 -0500

    Add flags

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
   "crypto/sha256"
   "encoding/hex"
+  "flag"
   "fmt"
 )
 
+var (
+  username = flag.String("username", "username", "What is the username to crack?")
+  salt     = flag.String("salt", "999999", "What is the salt to crack?")
+)
+
+func init() {
+  flag.Parse()
+}
+
 func main() {
   hash := sha256.New()
-  hash.Write([]byte("username,12345,999999"))
+  hash.Write([]byte(*username + ",12345," + *salt))
   md := hash.Sum(nil)
   mdStr := hex.EncodeToString(md)
   fmt.Printf(mdStr)
